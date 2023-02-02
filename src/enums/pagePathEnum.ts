bed0a821c48930becaf482937c5e375524a9067e
Luferov1 <ldima12022002@gmail.com>
2023-02-02 22:10:33 +0300

    feat: add pagePathEnum.ts

---
+++ src/enums/pagePathEnum.ts
@@ -0,0 +1,10 @@
+enum PagePath {
+  root = '/',
+  tripForm = 'trip',
+  trip = 'trip/:tripId',
+  offers = 'offers',
+  offerDescription = 'offers/:offerId',
+  profile = 'profile',
+}
+
+export default PagePath;
