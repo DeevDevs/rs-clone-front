9f1186b686ccb59abda29dff10157334c754cec2
Luferov1 <ldima12022002@gmail.com>
2023-02-02 22:48:31 +0300

    feat: add Routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Test from '../pages/Test';
-import Home from '../pages/Home';
+import ErrorPage from '../pages/rootPage/errorPage';
+import Root from '../pages/rootPage';
+import MainPage from '../pages/rootPage/mainPage';
+import PagePath from '../enums/pagePathEnum';
+import TripFormPage from '../pages/rootPage/tripFormPage';
+import TripPage from '../pages/rootPage/tripFormPage/tripPage';
+import OffersPage from '../pages/rootPage/offersPage';
+import OfferDescriptionPage from '../pages/rootPage/offersPage/offerDesctiptionPage';
+import ProfilePage from '../pages/rootPage/profilePage';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route
-        path=""
-        element={<Home />}
-      />
+        path={PagePath.root}
+        element={<Root />}
+      >
+        <Route index element={<MainPage />} />
+        <Route
+          path={PagePath.tripForm}
+          element={<TripFormPage />}
+        />
+        <Route
+          path={PagePath.trip}
+          element={<TripPage />}
+        />
+        <Route
+          path={PagePath.offers}
+          element={<OffersPage />}
+        />
+        <Route
+          path={PagePath.offerDescription}
+          element={<OfferDescriptionPage />}
+        />
+        <Route
+          path={PagePath.profile}
+          element={<ProfilePage />}
+        />
+      </Route>
       <Route
-        path="/test"
-        element={<Test />}
+        path={PagePath.error}
+        element={<ErrorPage />}
       />
     </Routes>
   </BrowserRouter>
