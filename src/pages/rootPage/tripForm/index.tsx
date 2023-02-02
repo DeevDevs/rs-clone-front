448806f9a5f2f4b6c538a26461f6dd6dd8830453
Luferov1 <ldima12022002@gmail.com>
2023-02-02 21:30:20 +0300

    feat: add new pages folders

---
+++ src/pages/rootPage/tripForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TripFormPage = () => (
+  <div>
+    This is trip form page
+  </div>
+);
+
+export default TripFormPage;
---
+++ src/pages/rootPage/tripPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TripPage = () => (
+  <div>
+    This is trip page
+  </div>
+);
+
+export default TripPage;
