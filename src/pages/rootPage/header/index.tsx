43a5a88a719689262a1ca964863ac294acbe4c19
Luferov1 <ldima12022002@gmail.com>
2023-02-04 16:35:27 +0300

    refactor: rename file in enums folder

--- src/pages/rootPage/header/index.tsx
+++ src/pages/rootPage/header/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './style.module.scss';
-import PagePathEnum from '../../../enums/pagePathEnum';
+import { PagePath } from '../../../enums';
 
 const Header = () => (
   <header className={styles.header}>
     <h2 className={styles.header__text}>Travel the world... travel the life!</h2>
     <div className={styles.links}>
       <NavLink
-        to={PagePathEnum.Root}
+        to={PagePath.Root}
         className={({ isActive }) => (isActive ? styles.button_active : styles.button)}
       >
         Home
       </NavLink>
       <NavLink
-        to={PagePathEnum.Offers}
+        to={PagePath.Offers}
         className={({ isActive }) => (isActive ? styles.button_active : styles.button)}
       >
         Offers
       </NavLink>
       <NavLink
-        to={PagePathEnum.Profile}
+        to={PagePath.Profile}
         className={styles.user}
       >
         <span className={styles.user__name}>John John</span>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ErrorPage from '../pages/rootPage/errorPage';
 import Root from '../pages/rootPage';
 import MainPage from '../pages/rootPage/mainPage';
-import PagePath from '../enums/pagePathEnum';
+import { PagePath } from '../enums';
 import TripFormPage from '../pages/rootPage/tripFormPage';
 import TripPage from '../pages/rootPage/tripFormPage/tripPage';
 import OffersPage from '../pages/rootPage/offersPage';
