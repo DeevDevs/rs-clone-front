06f3052063af36d933ea2972a9625e8ce98af9a2
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-08 16:24:22 +0400

    feat: add types for form fields

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,18 @@
+import React from 'react';
+
 export type WrapperProps = {
   children: React.ReactNode;
 };
+
+export type FormInputItems = {
+  memoir: string,
+  place: string,
+  country: string,
+  worldPart: string,
+  sight: string,
+  description: string,
+  dateFrom: Date;
+  dateTo: Date;
+};
+
+export type ValuesKey = keyof FormInputItems;
