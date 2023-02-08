cc4b8f07e69a940750422ea6e573a1601d87a766
Luferov1 <ldima12022002@gmail.com>
2023-02-08 18:32:01 +0300

    feat: add new enums

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -14,3 +14,75 @@ export enum Links {
   Kazakov = 'https://github.com/KazakovMaksim',
   Luferov = 'https://github.com/Luferov1',
 }
+
+export enum SignUpInputsNames {
+  Name = 'Name',
+  Email = 'SignUpEmail',
+  Password = 'SignUpPassword',
+  RepeatPassword = 'RepeatPassword',
+}
+
+export enum LoginInputsNames {
+  Email = 'LoginEmail',
+  Password = 'LoginPassword',
+}
+
+export enum SignUpLabels {
+  Name = 'Name',
+  Email = 'Email',
+  Password = 'Password',
+  RepeatPassword = 'Repeat Password',
+}
+
+export enum LoginLabels {
+  Email = 'Email',
+  Password = 'Password',
+}
+
+export enum SignUpLogin {
+  SignUp = 'Sign Up',
+  Login = 'Login',
+}
+
+export enum InputTypes {
+  Text = 'text',
+  Password = 'password',
+  Email = 'email',
+}
+
+export enum InputId {
+  SignUp1 = 'signup1',
+  SignUp2 = 'signup2',
+  SignUp3 = 'signup3',
+  SignUp4 = 'signup4',
+  Login1 = 'login1',
+  Login2 = 'login2',
+}
+
+export enum RequiredMessages {
+  Name = 'You must specify a name',
+  Email = 'You must specify an email',
+  Password = 'You must specify a password',
+}
+
+export enum LengthMessages {
+  Three = 'must have at least 3 symbols',
+  Eight = 'must have at least 8 symbols',
+  Thirty = 'must have maximum 30 symbols',
+}
+
+export enum PatternMessages {
+  Name = 'Name must contain only letters',
+  Email = 'Email must match email format',
+  Password = `Password must contain at least: 
+  1 lowercase alphabetical character, 
+  1 uppercase alphabetical character, 
+  1 numeric character`,
+}
+
+export enum ValidationTypes {
+  Required = 'required',
+  MaxLength = 'maxLength',
+  MinLength = 'minLength',
+  Pattern = 'pattern',
+}
