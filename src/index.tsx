938409fd84985dc9f69d2d9c4a0d94e69d111c46
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-09 19:57:15 +0400

    fix: update src index for redux settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { Provider } from 'react-redux';
-import store from './store';
+import { store } from './store';
 import Router from './router';
 import Wrapper from './components/wrapper';
 
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store()}>
       <Wrapper>
         <Router />
       </Wrapper>
