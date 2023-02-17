aaff2b058f4b8dc194898079b7e0a488bbee0d82
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-17 14:44:53 +0400

    feat: add Mapbox Component to trip page instead yandex map

--- src/pages/Root/TripPage/TripDetails/TripDetailsMap/index.tsx
+++ src/pages/Root/TripPage/TripDetails/TripDetailsMap/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import TripMap from '../../../TripFormPage/TripMapComp';
+import MainMap from '../../../MainPage/MainMap';
 import styles from './TripDetailsMap.module.scss';
 
 const TripDetailsMap = () => (
   <div className={styles.details_map}>
     <h3>Crossed 1415 kilometers</h3>
-    <TripMap />
+    <MainMap />
   </div>
 );
 
