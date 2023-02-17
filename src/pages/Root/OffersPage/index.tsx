781cb2ffdc71eeb086e65336f035ef3c88f0a54a
Luferov1 <ldima12022002@gmail.com>
2023-02-17 16:43:16 +0300

    feat: add Offers page

--- src/pages/Root/OffersPage/index.tsx
+++ src/pages/Root/OffersPage/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import TestPage from '../../../components/TempAllReqComponent';
+import styles from './style.module.scss';
+import toursStore from '../../../data/toursStore';
+import SwiperComp from './Swiper';
 
 const OffersPage = () => (
-  <div>
-    <TestPage />
-  </div>
+  <main className={styles.container}>
+    <h2 className={styles.header}>
+      Our team has prepared some trips and tours for you
+    </h2>
+    {toursStore.map(({ name, data }) => (
+      <div className={styles.swiperContainer}>
+        <h3 className={styles.continentHeader}>{name}</h3>
+        <SwiperComp tours={data} />
+      </div>
+    ))}
+  </main>
 );
 
 export default OffersPage;
