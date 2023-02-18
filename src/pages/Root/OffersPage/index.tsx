d3f217b02f5726a969712589bd91d6151ad9e283
Luferov1 <ldima12022002@gmail.com>
2023-02-18 12:11:08 +0300

    feat: add keys to mapped components

--- src/pages/Root/OffersPage/index.tsx
+++ src/pages/Root/OffersPage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './style.module.scss';
 import toursStore from '../../../data/toursStore';
 import SwiperComp from './Swiper';
+import keyGenerator from '../../../functions/index';
 
 const OffersPage = () => (
   <main className={styles.container}>
@@ -9,9 +10,14 @@ const OffersPage = () => (
       Our team has prepared some trips and tours for you
     </h2>
     {toursStore.map(({ name, data }) => (
-      <div className={styles.swiperContainer}>
+      <div
+        className={styles.swiperContainer}
+        key={keyGenerator('swiperComp')}
+      >
         <h3 className={styles.continentHeader}>{name}</h3>
-        <SwiperComp tours={data} />
+        <SwiperComp
+          tours={data}
+        />
       </div>
     ))}
   </main>
