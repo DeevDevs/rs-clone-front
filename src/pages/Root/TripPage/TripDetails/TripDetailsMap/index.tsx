5abe528ed2d5edc5a84579d6cc4ae69cd5e796c4
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-18 08:40:59 +0400

    feat: change Map component

--- src/pages/Root/TripPage/TripDetails/TripDetailsMap/index.tsx
+++ src/pages/Root/TripPage/TripDetails/TripDetailsMap/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import MainMap from '../../../MainPage/MainMap';
+import MapComponent from '../../../../../components/MapComponent';
 import styles from './TripDetailsMap.module.scss';
 
 const TripDetailsMap = () => (
   <div className={styles.details_map}>
     <h3>Crossed 1415 kilometers</h3>
-    <MainMap />
+    <MapComponent />
   </div>
 );
 
