7556876bcadbb84b7bcc2f27935d4a43f313544a
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-20 00:49:24 +0400

    feat: fill distance and story from memoir, add longLat and make the marker on map for the end point

--- src/pages/Root/TripPage/TripDetails/TripDetailsMap/index.tsx
+++ src/pages/Root/TripPage/TripDetails/TripDetailsMap/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import MapComponent from '../../../../../components/MapComponent';
+import { useAppSelector } from '../../../../../store';
+import { MapProps } from '../../../../../types';
 import styles from './TripDetailsMap.module.scss';
 
-const TripDetailsMap = () => (
-  <div className={styles.details_map}>
-    <h3>Crossed 1415 kilometers</h3>
-    <MapComponent />
-  </div>
-);
+const TripDetailsMap = () => {
+  const { distance, longLat, destinationName } = useAppSelector((state) => state.memoirReducer);
+  const [longTo, latTo] = longLat;
+  const locationTolong = longTo || 45.9809;
+  const locationToLat = latTo || 51.8531;
+
+  const newMapData: MapProps = {
+    newLocation: [locationTolong, locationToLat],
+    markerName: `Go to ${destinationName}`,
+  };
+  return (
+    <div className={styles.details_map}>
+      <h3>{`Crossed ${distance} kilometers`}</h3>
+      <MapComponent newMapInfo={newMapData} />
+    </div>
+  );
+};
 
 export default TripDetailsMap;
