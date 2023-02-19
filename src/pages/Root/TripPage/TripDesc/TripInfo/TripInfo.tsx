0a8fc92f68509272104476452a938e5c0a3a91d8
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-20 00:27:48 +0400

    feat: add base trip info from memoir on tripInfo section

--- src/pages/Root/TripPage/TripDesc/TripInfo/TripInfo.tsx
+++ src/pages/Root/TripPage/TripDesc/TripInfo/TripInfo.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import StatisticItem from '../../../../../components/StatisticItem';
 import styles from './TripInfo.module.scss';
 import { StatisticsItemsText } from '../../../../../enums';
+import { useAppDispatch, useAppSelector } from '../../../../../store';
+import { getMemoir } from '../../../../../store/memoir/memoirThunks';
+import months from '../../../../../constants/tripPage';
 
 const TripInfo = () => {
   const satisfaction = {
     maximum: 10,
     text: StatisticsItemsText.Satisfaction,
   };
+
+  const {
+    destinationName,
+    countryName,
+    continentName,
+    date,
+    rateValue,
+    days,
+    sites,
+  } = useAppSelector((state) => state.memoirReducer);
+
+  const newDate = new Date(date);
+  const tripDate = `${months[newDate.getMonth()]}, ${newDate.getFullYear()}`;
+  const memoirID = window.location.href.split('/').at(-1);
+  const dispatchApp = useAppDispatch();
+
+  const callbackGetMemoir = useCallback(async (memoirId: string) => {
+    await dispatchApp(getMemoir(memoirId));
+  }, []);
+
+  useEffect(() => {
+    if (memoirID) {
+      callbackGetMemoir(memoirID);
+    }
+  }, []);
+
   return (
     <div className={styles.trip_info}>
-      <h3>Edinburgh</h3>
-      <p>Scotland, Europe</p>
-      <p>October, 2021</p>
+      <h3>{destinationName}</h3>
+      <p>{`${countryName}, ${continentName}`}</p>
+      <p>{`On ${tripDate} ${days} days trip`}</p>
       <StatisticItem
-        mark={5}
+        mark={rateValue}
         maximum={satisfaction.maximum}
         text={satisfaction.text}
       />
-      <p>Destinations</p>
-      <p>Edinburgh Castle, Botanic Garden, National Galleries, Arthur’s Seat</p>
+      <p>Sites</p>
+      <p>{sites.join(', ')}</p>
     </div>
   );
 };
