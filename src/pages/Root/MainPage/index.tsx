702a4d862bc54c3274230c2ce5343adb34ee228d
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-24 18:38:01 +0400

    feat: add callPage changing on main page loading

--- src/pages/Root/MainPage/index.tsx
+++ src/pages/Root/MainPage/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SignUpLoginContainer from './SignUpLoginContainer';
 import Statistics from './Statistics';
 import styles from './style.module.scss';
-import { useAppSelector } from '../../../store';
+import { useAppDispatch, useAppSelector } from '../../../store';
+// import { useAppSelector } from '../../../store';
 
 import MainMap from './MainMap';
+import { mapboxActions } from '../../../store/mapbox';
 
 const MainPage = () => {
   const { id } = useAppSelector((state) => state.userReducer);
+  const dispatchApp = useAppDispatch();
+  const cbChangeCallMapboxState = (): void => {
+    dispatchApp(mapboxActions.changeCallPage('main'));
+  };
+
+  useEffect(() => {
+    cbChangeCallMapboxState();
+  }, []);
 
   return (
     <main className={styles.main}>
