b836620fa257f3816e8c0c01f5df48cf8a74cea0
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-26 13:20:48 +0400

    feat: add Block page from DV

--- src/pages/Root/TripFormPage/index.tsx
+++ src/pages/Root/TripFormPage/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import TripForm from './TripFormComp';
+import BlockPageComp from '../../../components/BlockPageComp';
+import { useAppSelector } from '../../../store';
 import style from './TripFormPageComp.module.scss';
 
-const TripFormPage = () => (
-  <main className={style.tripPage}>
-    <h2 className={style.tripPageTitle}>Writing a Memoir</h2>
-    <TripForm />
-  </main>
-);
+const TripFormPage = () => {
+  const { id } = useAppSelector((state) => state.userReducer);
+  return (
+    <main className={style.tripPage}>
+      {id
+        ? (
+          <>
+            <h2 className={style.tripPageTitle}>Writing a Memoir</h2>
+            <TripForm />
+          </>
+        )
+        : <BlockPageComp />}
+    </main>
+  );
+};
 
 export default TripFormPage;
