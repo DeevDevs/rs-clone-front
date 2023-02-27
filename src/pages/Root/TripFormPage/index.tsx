a6b8bc126b413df143b0c745219351f040ce15bd
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-27 23:53:08 +0400

    refactor: add rendering condition to TripFormPage

--- src/pages/Root/TripFormPage/index.tsx
+++ src/pages/Root/TripFormPage/index.tsx
@@ -6,13 +6,22 @@ import style from './TripFormPageComp.module.scss';
 
 const TripFormPage = () => {
   const { id } = useAppSelector((state) => state.userReducer);
+  const { clickLat, callPage } = useAppSelector((state) => state.mapboxReducer);
+
   return (
     <main className={style.tripPage}>
       {id
         ? (
           <>
             <h2 className={style.tripPageTitle}>Writing a Memoir</h2>
-            <TripForm />
+            {clickLat !== 0 || callPage === 'trip'
+              ? <TripForm />
+              : (
+                <div className={style.tripPage_error}>
+                  It seems that you want to record a new memoir.
+                  Please, go to main page to start.
+                </div>
+              )}
           </>
         )
         : <BlockPageComp />}
