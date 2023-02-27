760fc89f2226edf67d113a7b03ca662b1d1cbbb0
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-02-27 20:18:02 +0400

    fix: make error page appear inside header and footer

--- src/pages/Root/ErrorPage/index.tsx
+++ src/pages/Root/ErrorPage/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 
 const ErrorPage = () => (
   <main className={styles.container}>
-    Oops, these lands are yet to be discovered. Let&apos;s come back here later
+    Oops, these lands are yet to be discovered. Let&apos;s come back here later.
   </main>
 );
 
