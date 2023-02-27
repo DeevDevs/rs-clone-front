3a293c9bbcbf52896dc1f48b3b6fe6850db23d97
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-02-27 18:37:22 +0400

    feat: add simple slider to trip desc page

--- src/pages/Root/TripPage/TripDesc/TripSlider/index.tsx
+++ src/pages/Root/TripPage/TripDesc/TripSlider/index.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './TripSlider.module.scss';
-import photo from '../../../../../assets/img/temp-trip.jpg';
 import { useAppSelector } from '../../../../../store';
 
 const TripSlider = () => {
   const { memoirPhotos } = useAppSelector((state) => state.memoirReducer);
+  const [currIdx, setCurrIdx] = useState(0);
+
+  const goToPrev = () => {
+    if (currIdx === 0) {
+      setCurrIdx(memoirPhotos.length - 1);
+    } else {
+      setCurrIdx(currIdx - 1);
+    }
+  };
+
+  const goToNext = () => {
+    if (currIdx === memoirPhotos.length - 1) {
+      setCurrIdx(0);
+    } else {
+      setCurrIdx(currIdx + 1);
+    }
+  };
+
   return (
     <div className={styles.slider}>
-      <img src={(memoirPhotos[0] !== 'default.jpg') ? memoirPhotos[0] : photo} alt="" />
+      {memoirPhotos.length > 1 && (
+      <div
+        className={styles.slider_leftArrow}
+        role="presentation"
+        onClick={goToPrev}
+      />
+      )}
+      <div
+        style={
+          { backgroundImage: `url(${memoirPhotos[currIdx]})` }
+        }
+        className={styles.slider_imgContainer}
+      />
+      {memoirPhotos.length > 1 && (
+      <div
+        className={styles.slider_rightArrow}
+        role="presentation"
+        onClick={goToNext}
+      />
+      )}
     </div>
   );
 };
